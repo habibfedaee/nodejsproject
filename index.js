8a24b5d6ef553a594a5731348f40cdd29694c7aa
habibfedaee <habib.fedaee@gmail.com>
2023-05-31 01:05:46 -0700

    index.js cleaned

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 server.get("/hello", (req, res) => {
   res.json("hello, there");
+});
+
+server.use("*", (req, res) => {
+  res.json({
+    message: "API is UP!",
+  });
 });
 
 const port = process.env.PORT || 9000; // to use whatever port# heroku gives
 
 server.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
