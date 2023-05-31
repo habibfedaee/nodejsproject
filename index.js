a4fd3fc4014bf7364cfe3a0acf5d81cb40720266
habibfedaee <habib.fedaee@gmail.com>
2023-05-31 00:58:16 -0700

    heroku setup and main created

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const server = express();
 
-const port = process.env.port || 9000; // to use whatever port# heroku gives
-
 server.get("/hello", (req, res) => {
   res.json("hello, there");
 });
+
+const port = process.env.PORT || 9000; // to use whatever port# heroku gives
 
 server.listen(port, () => {
   console.log(`listening on port ${port}`);
